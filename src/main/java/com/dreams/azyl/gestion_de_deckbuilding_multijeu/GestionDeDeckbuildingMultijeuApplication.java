598fc1f41979e40075a90ff2b8ac788b6132d228
Toma <tomabarret@hotmail.com>
2025-05-20 12:54:33 +0400

    init projet

---
+++ src/main/java/com/dreams/azyl/gestion_de_deckbuilding_multijeu/GestionDeDeckbuildingMultijeuApplication.java
@@ -0,0 +1,13 @@
+package com.dreams.azyl.gestion_de_deckbuilding_multijeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeDeckbuildingMultijeuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDeDeckbuildingMultijeuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dreams/azyl/gestion_de_deckbuilding_multijeu/GestionDeDeckbuildingMultijeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreams.azyl.gestion_de_deckbuilding_multijeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeDeckbuildingMultijeuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
