89f2c10f72e951d4322c29e73da66baf5edf31f8
Toma <tomabarret@hotmail.com>
2025-05-21 09:01:22 +0400

    ajout getter/setter

--- src/main/java/com/dreams/azyl/gestion_de_deckbuilding_multijeu/model/CardCache.java
+++ src/main/java/com/dreams/azyl/gestion_de_deckbuilding_multijeu/model/CardCache.java
@@ -23,4 +23,55 @@ public class CardCache implements Serializable {
     private String description; // texte de la carte si tu veux l’afficher directement
 
     private LocalDateTime lastUpdated; // Pour gérer l’expiration ou le rafraîchissement du cache
+
+    public CardCache() {
+    }
+
+    public String getApiCardId() {
+        return apiCardId;
+    }
+
+    public void setApiCardId(String apiCardId) {
+        this.apiCardId = apiCardId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
 }
