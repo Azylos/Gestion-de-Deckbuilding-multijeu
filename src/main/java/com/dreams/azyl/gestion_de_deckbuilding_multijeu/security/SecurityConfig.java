09c31bb0fc04e6b23c91819414d70380f12b4d51
Toma <tomabarret@hotmail.com>
2025-05-21 18:05:05 +0400

    fix logout; a voir pour persistance connexion

--- src/main/java/com/dreams/azyl/gestion_de_deckbuilding_multijeu/security/SecurityConfig.java
+++ src/main/java/com/dreams/azyl/gestion_de_deckbuilding_multijeu/security/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig {
                         .permitAll()
                 )
                 .logout(logout -> logout
+                        .logoutUrl("/logout")
                         .logoutSuccessUrl("/")
                         .permitAll()
                 )
